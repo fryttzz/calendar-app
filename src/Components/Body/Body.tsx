52bdff13c32a54e81bd56e31fc285b7b330e60ef
Matheus Santos <matheusps0012@gmail.com>
2021-10-06 15:50:18 -0300

    Add - Highlight css classes

--- src/Components/Body/Body.tsx
+++ src/Components/Body/Body.tsx
@@ -1,64 +1,11 @@
 import "./Body.css";
-import CardDay from "../CardDay/CardDay";
+import WeeksRows from "../WeeksRows/WeeksRows";
 import DaysRow from "../DaysRow/DaysRow";
 const Body = () => {
   return (
     <div id="Body">
       <DaysRow />
-      <div id="RowWeek1" className="RowWeek">
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-      </div>
-      <div id="RowWeek2" className="RowWeek">
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-      </div>
-      <div id="RowWeek3" className="RowWeek">
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-      </div>
-      <div id="RowWeek4" className="RowWeek">
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-      </div>
-      <div id="RowWeek5" className="RowWeek">
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-      </div>
-      <div id="RowWeek6" className="RowWeek">
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-      </div>
+      <WeeksRows />
     </div>
   );
 };
---
+++ src/Components/WeeksRows/WeeksRows.tsx
@@ -0,0 +1,64 @@
+import "./WeeksRows.css";
+import CardDay from "../CardDay/CardDay";
+const WeeksRows = () => {
+  return (
+    <div id="WeeksRows">
+      <div id="RowWeek1" className="RowWeek">
+        <CardDay />
+        <CardDay />
+        <CardDay />
+        <CardDay />
+        <CardDay />
+        <CardDay />
+        <CardDay />
+      </div>
+      <div id="RowWeek2" className="RowWeek">
+        <CardDay />
+        <CardDay />
+        <CardDay />
+        <CardDay />
+        <CardDay />
+        <CardDay />
+        <CardDay />
+      </div>
+      <div id="RowWeek3" className="RowWeek">
+        <CardDay />
+        <CardDay />
+        <CardDay />
+        <CardDay />
+        <CardDay />
+        <CardDay />
+        <CardDay />
+      </div>
+      <div id="RowWeek4" className="RowWeek">
+        <CardDay />
+        <CardDay />
+        <CardDay />
+        <CardDay />
+        <CardDay />
+        <CardDay />
+        <CardDay />
+      </div>
+      <div id="RowWeek5" className="RowWeek">
+        <CardDay />
+        <CardDay />
+        <CardDay />
+        <CardDay />
+        <CardDay />
+        <CardDay />
+        <CardDay />
+      </div>
+      <div id="RowWeek6" className="RowWeek">
+        <CardDay />
+        <CardDay />
+        <CardDay />
+        <CardDay />
+        <CardDay />
+        <CardDay />
+        <CardDay />
+      </div>
+    </div>
+  );
+};
+
+export default WeeksRows;
