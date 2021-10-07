6bcd5ea39304d03d0a933b3dcd1800d8408b8d98
Matheus Santos <matheusps0012@gmail.com>
2021-10-07 16:07:47 -0300

    Add - Calendar Style

--- src/Components/WeeksRows/WeeksRows.tsx
+++ src/Components/WeeksRows/WeeksRows.tsx
@@ -7,8 +7,12 @@ const WeeksRows = () => {
     .format("YYYY-MM-DD");
   var nextMonth = moment().add(1, "M").startOf("month").format("YYYY-MM-DD");
   var daysInMonth = moment().daysInMonth();
-  var fullMonth: Array<{ dayN: string; dayT: string }> = [];
-  var previousMonthLastDays: Array<{ dayN: string; dayT: string }> = [];
+  var fullMonth: Array<{ dayN: string; dayT: string; current: boolean }> = [];
+  var previousMonthLastDays: Array<{
+    dayN: string;
+    dayT: string;
+    current: boolean;
+  }> = [];
   var nextMonthFirtDays = [];
   var startOfMonth = moment().startOf("month").format("YYYY-MM-DD");
 
@@ -16,6 +20,7 @@ const WeeksRows = () => {
     fullMonth.push({
       dayN: moment(startOfMonth).format("DD"),
       dayT: moment(startOfMonth).format("d"),
+      current: true,
     });
     startOfMonth = moment(startOfMonth).add(1, "d").format("YYYY-MM-DD");
   }
@@ -25,6 +30,7 @@ const WeeksRows = () => {
       previousMonthLastDays.push({
         dayN: moment(previousMonth).format("DD"),
         dayT: moment(previousMonth).format("d"),
+        current: false,
       });
       previousMonth = moment(previousMonth)
         .subtract(1, "d")
@@ -42,12 +48,14 @@ const WeeksRows = () => {
       nextMonthFirtDays.push({
         dayN: moment(nextMonth).format("DD"),
         dayT: moment(nextMonth).format("d"),
+        current: false,
       });
       nextMonth = moment(nextMonth).add(1, "d").format("YYYY-MM-DD");
     }
   }
 
   fullMonth = previousMonthLastDays.concat(fullMonth).concat(nextMonthFirtDays);
+  console.log(fullMonth);
   const weekRows = [1, 2, 3, 4, 5, 6];
   const daysColumns = [1, 2, 3, 4, 5, 6, 7];
   var indexFullMonth = -1;
@@ -58,11 +66,19 @@ const WeeksRows = () => {
           <div id={"RowWeek" + i} className="RowWeek" key={i}>
             {daysColumns.map((days) => {
               indexFullMonth++;
-              return (
-                <div className="CardDay">
-                  <span>{fullMonth[indexFullMonth].dayN}</span>
-                </div>
-              );
+              if (fullMonth[indexFullMonth].current === false) {
+                return (
+                  <div className="CardNotCurrentDay" key={`CardDay${indexFullMonth}`}>
+                    <span>{fullMonth[indexFullMonth].dayN}</span>
+                  </div>
+                );
+              } else {
+                return (
+                  <div className="CardDay" key={`CardDay${indexFullMonth}`}>
+                    <span>{fullMonth[indexFullMonth].dayN}</span>
+                  </div>
+                );
+              }
             })}
           </div>
         );
