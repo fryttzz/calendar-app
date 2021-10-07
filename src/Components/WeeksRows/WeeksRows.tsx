5a5d051c4976301d2e24de7003b1f6e4b47a96ca
Matheus Santos <matheusps0012@gmail.com>
2021-10-07 14:55:39 -0300

    Done 42 days object

--- src/Components/WeeksRows/WeeksRows.tsx
+++ src/Components/WeeksRows/WeeksRows.tsx
@@ -1,62 +1,69 @@
 import "./WeeksRows.css";
 import CardDay from "../CardDay/CardDay";
+import moment from "moment";
 const WeeksRows = () => {
+  var previousMonth = moment()
+    .subtract(1, "M")
+    .endOf("month")
+    .format("YYYY-MM-DD");
+  var nextMonth = moment().add(1, "M").startOf("month").format("YYYY-MM-DD");
+  var daysInMonth = moment().daysInMonth();
+  var fullMonth = [];
+  var previousMonthLastDays: Array<{ dayN: string; dayT: string }> = [];
+  var nextMonthFirtDays = [];
+  var startOfMonth = moment().startOf("month").format("YYYY-MM-DD");
+
+  for (let index = 0; index < daysInMonth; index++) {
+    fullMonth.push({
+      dayN: moment(startOfMonth).format("DD"),
+      dayT: moment(startOfMonth).format("d"),
+    });
+    startOfMonth = moment(startOfMonth).add(1, "d").format("YYYY-MM-DD");
+  }
+
+  if (fullMonth[0].dayT !== "0") {
+    for (let index = 0; index < parseInt(fullMonth[0].dayT); index++) {
+      previousMonthLastDays.push({
+        dayN: moment(previousMonth).format("DD"),
+        dayT: moment(previousMonth).format("d"),
+      });
+      previousMonth = moment(previousMonth)
+        .subtract(1, "d")
+        .format("YYYY-MM-DD");
+    }
+    previousMonthLastDays = previousMonthLastDays.reverse();
+  }
+
+  if (fullMonth[fullMonth.length - 1].dayT !== "6") {
+    for (
+      let index = 6;
+      index > parseInt(fullMonth[fullMonth.length - 1].dayT);
+      index--
+    ) {
+      nextMonthFirtDays.push({
+        dayN: moment(nextMonth).format("DD"),
+        dayT: moment(nextMonth).format("d"),
+      });
+      nextMonth = moment(nextMonth).add(1, "d").format("YYYY-MM-DD");
+    }
+  }
+
+  fullMonth = previousMonthLastDays.concat(fullMonth).concat(nextMonthFirtDays);
+  const weekRows = [1, 2, 3, 4, 5, 6, 7];
+  console.log(fullMonth);
   return (
     <div id="WeeksRows">
-      <div id="RowWeek1" className="RowWeek">
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-      </div>
-      <div id="RowWeek2" className="RowWeek">
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-      </div>
-      <div id="RowWeek3" className="RowWeek">
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-      </div>
-      <div id="RowWeek4" className="RowWeek">
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-      </div>
-      <div id="RowWeek5" className="RowWeek">
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-      </div>
-      <div id="RowWeek6" className="RowWeek">
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-        <CardDay />
-      </div>
+      {weekRows.map((i) => (
+        <div id={"RowWeek" + i} className="RowWeek" key={i}>
+          <CardDay />
+          <CardDay />
+          <CardDay />
+          <CardDay />
+          <CardDay />
+          <CardDay />
+          <CardDay />
+        </div>
+      ))}
     </div>
   );
 };
