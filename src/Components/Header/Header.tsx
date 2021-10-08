ab6e6618bb7363744531fc4ca2185fddf6ace1f3
Matheus Santos <matheusps0012@gmail.com>
2021-10-08 10:38:27 -0300

    Done - Ready to refactorization

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -2,8 +2,6 @@ import "./Header.css";
 import moment from "moment";
 import "moment/locale/pt-br";
 const Header = () => {
-  //var currentMonth = moment().format("YYYY-MM-DD");
-
   return (
     <div id="Header">
       <span>{moment().format("MMMM").toUpperCase()}</span>
